934cf148a1709ebe1aa0a8203baef891cd35dea0
Nate W <nwillett@student.neumont.edu>
2025-11-18 20:21:08 -0700

    Created basic pages.

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <nav class="nav-bar-container">
+            <NuxtLink class="nav-bar" to="/">Home</NuxtLink>
+            <NuxtLink class="nav-bar" to="/login">Log in</NuxtLink>
+            <NuxtLink class="nav-bar" to="/signup">Sign up</NuxtLink>
+            <NuxtLink class="nav-bar" to="/messageboard">Board</NuxtLink>
+        </nav>
+        <slot />
+        <h4>© Copyright 200-2025 Nathan & Nathan Attorneys Agenst Law</h4>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <main>
+        Hog Tuah
+    </main>
+</template>
+
+<style src="assets/css/app.css"/>
