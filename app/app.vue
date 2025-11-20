4ea60400f6a6dbf7193d03e10399e188972265db
GabrielGarcia984 <157398987+GabrielGarcia984@users.noreply.github.com>
2025-11-19 18:25:23 -0700

    made a little pretty

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Eagle+Lake&display=swap');
+.rule {
+  font-family: "Eagle Lake", serif;
+  font-weight: 400;
+  font-style: normal;
+  color: red;
+  text-align: center;
+  background-image: url("~/img/oldPaper.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="rule">Test rule</h1>
   </div>
 </template>
