d04b57835df27a4d127b5d132788955ba419f762
GabrielGarcia984 <157398987+GabrielGarcia984@users.noreply.github.com>
2025-11-20 19:45:04 -0700

    made a little pretty

--- app/pages/messageboard.vue
+++ app/pages/messageboard.vue
@@ -1,5 +1,54 @@
+<!--"npm run dev" to run page-->
+
+
 <template>
-    <main>
-        
-    </main>
+  <div class="main_board">
+  <header>
+    <h1 class="rule"> Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule</h1>
+  </header>
+  <div>
+  <p class="posted">
+    Here is a concise post crafted to be exactly two hundred characters long, giving you a clear, simple message that is useful, readable, and neatly contained within the limit Enjoy this extra note here!  </p>
+  <p class="posted">
+    test
+  </p>
+  <p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p>
+  <p class="posted">
+    test
+  </p>
+  <p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p><p class="posted">
+    test
+  </p>
+  </div>
+  <footer>
+    <p class="posting">    Here is a concise post crafted to be exactly two hundred characters long, giving you a clear, simple message that is useful, readable, and neatly contained within the limit Enjoy this extra note here!  </p>
+
+  </footer>
+  </div>
 </template>
+<script setup lang="ts">
+</script>
