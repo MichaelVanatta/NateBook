f8dc457e1b7fa5b4d6670a9a9db76f0929bfa566
IceEmperor4944 <macaroni49344@gmail.com>
2025-11-25 20:16:53 -0700

    Made the shit do the shit

--- app/pages/messageboard.vue
+++ app/pages/messageboard.vue
@@ -2,64 +2,76 @@
 
 <template>
   <div class="main_board">
-  <header>
-    <h1 class="rule"> Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule</h1>
-  </header>
-  <div>
-  <p class="posted">
-    Here is a concise post crafted to be exactly two hundred characters long, giving you a clear, simple message that is useful, readable, and neatly contained within the limit Enjoy this extra note here!  </p>
-  <p class="posted">
-    test
-  </p>
-  <p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p>
-  <p class="posted">
-    test
-  </p>
-  <p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p><p class="posted">
-    test
-  </p>
-  </div>
-  <footer>
-    <p name="input" class="posting">
-      Here is a concise post crafted to be exactly two hundred characters long, giving you a clear, simple message that is useful, readable, and neatly contained within the limit Enjoy this extra note here!
+    <header>
+      <h1 class="rule"> Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule Test rule</h1>
+    </header>
+    <div>
+    <p class="posted">
+      Here is a concise post crafted to be exactly two hundred characters long, giving you a clear, simple message that is useful, readable, and neatly contained within the limit Enjoy this extra note here!  </p>
+    <p class="posted">
+      test
     </p>
-    <UButton icon="send-button.png" loading-auto @click="readInput" />
-  </footer>
-  </div>
-</template>
-<script setup lang="ts">
-import { ref } from 'vue'
-import {User} from "../objects/userPost"
-const users = ref<User[]>([])
-//read from database
+    <p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p>
+    <p class="posted">
+      test
+    </p>
+    <p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p><p class="posted">
+      test
+    </p>
+    </div>
+    <footer>
+      <input name="input" class="posting" placeholder="Add your post following the RULE">
+        Here is a concise post crafted to be exactly two hundred characters long, giving you a clear, simple message that is useful, readable, and neatly contained within the limit Enjoy this extra note here!
+      </input>
+      <UButton icon="send-button.png" loading-auto @click="readInput" />
+    </footer>
+    </div>
+  </template>
+  <script setup lang="ts">
+  import {User} from "../objects/userPost"
+  import {Post} from "../objects/userPost"
+  const users: User[] = []
+  const posts: Post[] = []
+  //read from database
 
+  let text: string = "";
+  var u: User = new User(1, "pluh", "ffffff")
+  //get user ID
 
+  users.push(u)
+
+posts.forEach(element => {
+    const newPost = document.createElement('p');
+    var name = users.find(x => x.id == element.userId)?.username
+    newPost.textContent = name + element.text
+});
 
 async function readInput() {
-
+var post: Post = new Post(text, u.id)
+posts.push(post)
 }
 </script>
