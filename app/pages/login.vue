d7dc93e7bc8fbb1597d1818b52ca2349092dd38d
Michael Vanatta <michaelvanatta12@gmail.com>
2025-11-25 20:10:44 -0700

    Fixed signup and login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -18,27 +18,23 @@ import {ref} from "vue";
     
 <template>
 <main class="login_signUp">
-      <form action="action_page.php" >
-        <div class="container">
-          <h1>Login Now</h1>
+    <div class="container">
+        <h1>Login Now</h1>
 
 
-          <label for="name"><b>UserName</b></label> <br>
-          <input v-model="username" placeholder="Enter UserName" name="name" required> <br>
+        <label for="name"><b>UserName</b></label> <br>
+        <input v-model="username" placeholder="Enter UserName" name="name" required> <br>
 
 
-          <label for="psw-repeat"><b> Password</b></label> <br>
-          <input v-model="password" placeholder="Enter Password" name="psw" required>
+        <label for="psw-repeat"><b> Password</b></label> <br>
+        <input v-model="password" placeholder="Enter Password" name="psw" required>
 
-          <div class="clearfix">
+        <div class="clearfix">
             <button type="button" class="cancelbtn">Cancel</button>
-            <button type="submit" class="loginbtn">login</button>
-            <button @click="displayInfo">Submit</button>
-
-        <p>{{ info }}</p>
-          </div>
+            <button type="submit" class="loginbtn" @click="checkAccount">login</button>
+            <!-- <button @click="displayInfo">Submit</button> -->
         </div>
-      </form>
+    </div>
 </main>
 </template>
 <style src="assets/css/app.css"/>
