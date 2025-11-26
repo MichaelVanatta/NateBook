074021643a79653816f568180f480c3d16e4468d
GabrielGarcia984 <157398987+GabrielGarcia984@users.noreply.github.com>
2025-11-25 19:23:27 -0700

    made a little pretty

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,5 +1,24 @@
 <template>
-    <main>
-        
+    <main class="login_signUp">
+      <form action="action_page.php" >
+        <div class="container">
+          <h1>Login Now</h1>
+
+
+          <label for="name"><b>UserName</b></label> <br>
+          <input type="text" placeholder="Enter UserName" name="name" required> <br>
+
+
+          <label for="psw-repeat"><b> Password</b></label> <br>
+          <input type="password" placeholder="Enter Password" name="psw" required>
+
+          <div class="clearfix">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <button type="submit" class="loginbtn">login</button>
+          </div>
+        </div>
+      </form>
     </main>
 </template>
+<script setup lang="ts">
+</script>
--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -1,5 +1,26 @@
 <template>
-    <main>
-        
+    <main class="login_signUp">
+      <form action="action_page.php" >
+        <div class="container">
+          <h1>Sign Up Now</h1>
+          <p>Fill in this form to join the board.</p>
+          <hr>
+
+          <label for="name"><b>UserName</b></label> <br>
+          <input type="text" placeholder="Enter UserName" name="name" required> <br>
+
+
+          <label for="psw-repeat"><b> Password</b></label> <br>
+          <input type="password" placeholder="Enter Password" name="psw" required>
+
+          <div class="clearfix">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <button type="submit" class="signupbtn">Sign Up</button>
+          </div>
+        </div>
+      </form>
     </main>
 </template>
+<script setup lang="ts">
+
+</script>
